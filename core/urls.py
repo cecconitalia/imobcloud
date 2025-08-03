5b9d65a98bb33101d2f7774c37ceb269488d020c
Cristiano Ceccon <cecconitalia@gmail.com>
2025-08-03 21:05:52 +0200

    Removendo arquivos do venv e aplicando corretamente o .gitignore

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-# C:\wamp64\www\ImobCloud\core\urls.py
-
-from django.urls import path
-from .views import UserProfileView
-
-urlpatterns = [
-    path('profile/', UserProfileView.as_view(), name='user_profile'),
-]
