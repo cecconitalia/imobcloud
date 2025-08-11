eccb6414b7495073c6ba886e2f43f63c0e92bfe6
Cristiano Ceccon <cecconitalia@gmail.com>
2025-08-11 19:12:20 +0200

    Limpando e reaplicando .gitignore

--- app_financeiro/apps.py
+++
@@ -1,7 +0,0 @@
-# app_financeiro/apps.py
-
-from django.apps import AppConfig
-
-class AppFinanceiroConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_financeiro'
